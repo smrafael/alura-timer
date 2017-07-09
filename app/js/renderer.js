44753d2ee2c499f3bb6a70d3c3acf05c86ff006b
Rafael Mendes <rafael@Rafaels-Mac-mini.local>
2017-07-09 00:02:10 -0300

    new layout; play/pause implemented

--- app/js/renderer.js
+++ app/js/renderer.js
@@ -1,1 +1,22 @@
-console.log('Hello Console!');
+const { ipcRenderer } = require('electron');
+const timer = require('./timer');
+
+let linkSobre = document.querySelector('#link-sobre');
+linkSobre.addEventListener('click' , function(){
+    ipcRenderer.send('abrir-janela-sobre');
+});
+
+let imgs = ['img/play-button.svg', 'img/stop-button.svg'];
+let botaoPlay = document.querySelector('.botao-play');
+let tempo = document.querySelector('.tempo');
+let play = false;
+botaoPlay.addEventListener('click', () => {
+    if (play) {
+        timer.parar();
+    } else {
+        timer.iniciar(tempo);
+    }
+    play = !play;
+    imgs.reverse();
+    botaoPlay.src = imgs[0];
+});
---
+++ app/js/timer.js
@@ -0,0 +1,21 @@
+const moment = require('moment');
+let segundos;
+let timer;
+
+module.exports = {
+    iniciar(el) {
+        let tempo = moment.duration(el.textContent);
+        segundos = tempo.asSeconds();
+        clearInterval(timer);
+        timer = setInterval(() => {
+            segundos++
+            el.textContent = this.segundosParaTempo(segundos);
+        }, 1000);
+    },
+    parar() { 
+        clearInterval(timer);
+    },
+    segundosParaTempo() {
+        return moment().startOf('day').seconds(segundos).format("HH:mm:ss");
+    }
+}
